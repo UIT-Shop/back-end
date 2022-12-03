023261b6795fe4328dcc8200c5281dfbe11fb181
Nguyen Duy Phuc <khidetabino103@gmail.com>
2022-12-03 14:02:45 +0700

    Fake data, Image Api

---
+++ MyShop/Models/ProductColor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyShop.Models
+{
+    public class ProductColor
+    {
+        public string Id { get; set; }
+        [Column(TypeName = "nvarchar(MAX)")]
+        public string Name { get; set; }
+
+        public List<Image> Images { get; set; } = new List<Image>();
+    }
+}
